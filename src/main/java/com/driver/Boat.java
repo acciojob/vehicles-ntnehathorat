5d6247e68775590bf3df110dae0dc4f5944b51ba
Neha Thorat <nehathorat2016@gmail.com>
2023-08-17 18:21:27 +0530

    Task Compleated

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,8 +1,14 @@
 package com.driver;
 
 public class Boat implements WaterVehicle {
+
     private String name;
     private int capacity;
+
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
 
     public String getName() {
         return name;
